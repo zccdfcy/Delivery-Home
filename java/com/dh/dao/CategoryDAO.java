4e40dcef3c15c7b006be5cc589050613c6d9901b
zccdfcy <55950547+zccdfcy@users.noreply.github.com>
2021-08-09 13:57:03 -0700

    Delete java/com/dh directory

--- java/com/dh/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dh.dao;
-  
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-import com.dh.pojo.Category;
- 
-public interface CategoryDAO extends JpaRepository<Category,Integer>{
- 
-}
